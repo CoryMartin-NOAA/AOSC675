0feb086149ee775c72f7f8a345187ad095683025
Cory Martin <cmartin@atmos.umd.edu>
2015-04-01 13:25:51 -0400

    Driver File Created / Doxygen
    Sample Driver File created, with basic doxygen for documentation

---
+++ driver.f90
@@ -0,0 +1,19 @@
+!--------------------------------------------------------------------
+! AOSC 675 Final Project
+!--------------------------------------------------------------------
+!
+! PROGRAM: Driver
+!
+!> @author
+!> Doyeon Ahn, Cory Martin & Sandra Roberts
+!
+! DESCRIPTION:
+!> Driver for simple plant photosynthesis and soil respiration model.
+!
+! REVISION HISTORY:
+! Apr 2015 - Initial Version
+!--------------------------------------------------------------------
+program driver
+implicit none
+
+end program
